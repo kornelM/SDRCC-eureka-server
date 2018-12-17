7718cbdc7edb5f1a377b0173d25a23becfb34f17
Kornel <makusz07@gmail.com>
2018-12-17 22:46:47 +0100

    Initial commit, add application to repository
    Application works correctly, after some troubles with proper configuration

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Self driving RC project
+
+App Eureka-Server keeps configuration for all other SelfDrivingRCCar project's components.<br/>
+
+
+## Author
+
+**Korneliusz Mikołajski**
---
+++ src/main/java/com/myapp/eureka/SelfDrivingCarEureka/SelfDrivingCarEurekaServer.java
@@ -0,0 +1,16 @@
+package com.myapp.eureka.SelfDrivingCarEureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaServer
+public class SelfDrivingCarEurekaServer {
+    public static void main(String[] args) {
+        SpringApplication.run(SelfDrivingCarEurekaServer.class, args);
+    }
+}
